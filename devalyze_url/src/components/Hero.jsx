9e29f7138b01f3a0ddf15a64aeec6675123aef25
Yulong-Dev <onyiigodset@gmail.com>
2025-08-01 22:38:28 +0100

    update-4

--- devalyze_url/src/components/Hero.jsx
+++ devalyze_url/src/components/Hero.jsx
@@ -34,7 +34,7 @@ export default function Hero() {
     try {
       const endpoint = activeTab === "short" ? "shorten" : "qr";
 
-      const res = await fetch(`http://localhost:5000/${endpoint}`, {
+      const res = await fetch(`https://devalyze-url-shortener.onrender.com/${endpoint}`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ longUrl }),
