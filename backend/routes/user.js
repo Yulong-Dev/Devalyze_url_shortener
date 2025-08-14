fe4a390e8b179b08c80fa8e7c3a6df6a2a97e268
Yulong-Dev <onyiigodset@gmail.com>
2025-08-14 02:04:56 +0100

    signup working

---
+++ backend/routes/user.js
@@ -0,0 +1,19 @@
+// routes/user.js
+const express = require("express");
+const router = express.Router();
+const verifyToken = require("../middleware/authMiddleware");
+const User = require("../models/User");
+
+router.get("/me", verifyToken, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.userId).select("-password"); // exclude password
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    res.json(user);
+  } catch (err) {
+    res.status(500).json({ message: "Server error", error: err.message });
+  }
+});
+
+module.exports = router;
