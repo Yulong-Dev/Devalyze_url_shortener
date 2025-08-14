1164598040cca34a3ccc1880589fa567aa44da54
Bankole Ifeoluwa <ifeoluwa.bankole05@gmail.com>
2025-08-14 21:09:38 +0100

    Responsiveness of the SignIn and SignUp

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,11 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: "https://devalyze-url-shortener.vercel.app", // frontend URL
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true,
+}));
 
 
 // Parse JSON bodies
